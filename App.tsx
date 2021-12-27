70b0c9d045aadd4be9f67f9ac911f73f3803cb59
joel stevick <joelstevick@outlook.com>
2021-12-26 20:18:41 -0500

    set up redux store

--- App.tsx
+++ App.tsx
@@ -4,6 +4,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { createStore, combineReducers } from 'redux'
+import { authReducer } from './store/reducers/auth/auth.reducer'
+import { Provider } from 'react-redux'
+
+const rootReducer = combineReducers({
+  auth: authReducer
+})
+const store = createStore(rootReducer)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,10 +21,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </Provider>
     );
   }
 }
---
+++ store/reducers/auth/auth.reducer.ts
@@ -0,0 +1,10 @@
+interface AuthState {
+    loggedIn: boolean
+}
+
+const initialState: AuthState = {
+    loggedIn: false
+}
+export const authReducer = (state: AuthState = initialState, action: any) => {
+    return state;
+}
